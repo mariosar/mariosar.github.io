a7d7a20d8a1bcdf9c3269872975f0b7102551288
Mario Saraiva <mario.s.saraiva@gmail.com>
2019-10-07 15:34:38 -0400

    Importing background styler from npm.

--- webpack/entry.js
+++ webpack/entry.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ContactForm from '../src/contact_form'
 
+import movingBG from 'background-styler';
+
+movingBG().run()
+
 ReactDOM.render(<ContactForm />, document.getElementById('contact-form'));
