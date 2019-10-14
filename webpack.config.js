fe8cca92d5834c00f652649b539cae48fa8925ba
Mario Saraiva <mario.s.saraiva@gmail.com>
2019-10-14 16:50:14 -0400

    Added favicon.

--- webpack.config.js
+++ webpack.config.js
@@ -7,18 +7,18 @@ module.exports = {
     filename: "bundle.js"
   },
   module: {
-  rules: [
-    {
-      test: /\.(js|jsx)?$/,
-      exclude: /(node_modules)/,
-      loader: "babel-loader",
-      query: {
-        presets: [
-          "@babel/preset-env",
-          "@babel/preset-react"
-        ]
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+        query: {
+          presets: [
+            "@babel/preset-env",
+            "@babel/preset-react"
+          ]
+        }
       }
-    }
     ]
   }
 };
